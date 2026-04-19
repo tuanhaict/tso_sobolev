880f7cab2075126ad478f53c544059326c1c4271
Hoang Anh Tuan <tuanhoanganh.ict@gmail.com>
2026-04-19 09:14:22 +0700

    update

--- experiments/gradient-flow/GradientFlow.py
+++ experiments/gradient-flow/GradientFlow.py
@@ -20,7 +20,7 @@ seeds = range(1,args.num_seeds+1)
 modes = ['linear', 'linear', 'linear', 'linear', 'linear', 'linear', 'linear', 'linear']
 # titles = ['OSbTSW', 'TSW-SL-distance-based', 'TSW-SL-uniform', 'SbTS', 'LCVSW', 'SWGG', 'MaxSW', 'SW']
 # titles = ['OSbTSW', 'TSW-SL-distance-based', 'NTSW', 'TSW-SL-uniform', 'SbTS', 'LCVSW', 'SWGG', 'SW']
-titles = ['OSbTSW', 'TSW-SL-distance-based', 'NTSW', 'TSW-SL-uniform', 'SbTS']
+titles = ['OSbTSW', 'TSW-SL-distance-based']
 colors = ['blue', 'orange', 'red', 'green', 'purple', 'brown', 'pink', 'cyan', 'magenta']
 
 # Arrays to store results
