137516c32d09be29ffcd5642f3d059a3f3d19de5
Hoang Anh Tuan <tuanhoanganh.ict@gmail.com>
2026-04-28 20:35:20 +0700

    update

--- spherical/methods/osbstsd.py
+++ spherical/methods/osbstsd.py
@@ -327,8 +327,8 @@ def unif_hypersphere(shape, device):
     samples = F.normalize(samples, p=2, dim=-1)
     return samples
 
-def osbsts(X, Y, ntrees=250, nlines=4, p=2, delta=2, device='cuda', type='normal', n_function="power", p_agg=2):
-    TW_obj = OSbSTSD(ntrees=ntrees, nlines=nlines, p=p, delta=delta, device=device, type=type, n_function=n_function, p_agg=p_agg)
+def osbsts(X, Y, ntrees=250, nlines=4, p=2, delta=2, device='cuda', type='normal', n_function="power", p_agg=2, optimization_method="bounded"):
+    TW_obj = OSbSTSD(ntrees=ntrees, nlines=nlines, p=p, delta=delta, device=device, type=type, n_function=n_function, p_agg=p_agg, optimization_method=optimization_method)
     stswd = TW_obj(X, Y)
     return stswd
 
