dadcb7b0994361aa43c56e981d45320f616ba8d5
Hoang Anh Tuan <tuanhoanganh.ict@gmail.com>
2026-04-28 16:32:25 +0700

    update

--- db_tsw/osb_tsw.py
+++ db_tsw/osb_tsw.py
@@ -212,7 +212,7 @@ class OSb_TSConcurrentLines:
                 # init k using inverse mean scale
                 k = 1.0 / (h_flat.mean() + 1e-8)
 
-                for _ in range(30): 
+                for _ in range(10): 
                     kh = k * h_flat
 
                     Phi = self.n_function(kh)
