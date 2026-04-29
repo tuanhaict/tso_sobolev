701f715391aa45b1b8b2b5c7563086d0ee0bcecb
Hoang Anh Tuan <tuanhoanganh.ict@gmail.com>
2026-04-29 14:06:08 +0700

    update

--- experiments/gradient-flow/GradientFlow.py
+++ experiments/gradient-flow/GradientFlow.py
@@ -20,7 +20,7 @@ seeds = range(1,args.num_seeds+1)
 modes = ['linear', 'linear', 'linear', 'linear', 'linear', 'linear', 'linear', 'linear']
 # titles = ['OSbTSW', 'TSW-SL-distance-based', 'TSW-SL-uniform', 'SbTS', 'LCVSW', 'SWGG', 'MaxSW', 'SW']
 # titles = ['OSbTSW', 'NTSW','TSW-SL-distance-based', 'TSW-SL-uniform', 'SbTS', 'LCVSW', 'SWGG', 'SW']
-titles = ['Max-SW']
+titles = ['OSbTSW']
 colors = ['blue', 'orange', 'red', 'green', 'purple', 'brown', 'pink', 'cyan', 'magenta']
 
 # Arrays to store results
@@ -86,7 +86,7 @@ for k, title in enumerate(titles):
             
             loss = 0
 
-            if k == 6:
+            if k == 0:
                 start_time = time.time()  # Start timing
                 theta_twd, intercept_twd = generate_trees_frames(
                     ntrees=int(args.L / args.n_lines),
@@ -179,7 +179,7 @@ for k, title in enumerate(titles):
                 loss += gsw_res.sw(X.to(device), Y, theta=None)
                 end_time = time.time()
                 # print(f"Time taken for TWD orthogonal: {end_time - start_time:.4f} seconds")
-            elif k == 0:
+            elif k == 6:
                 start_time = time.time()  # Start timing
                 l, theta, loss_max = gsw_res.max_sw(X.to(device), Y, iterations=100, lr=lear_rates[k])
                 loss += l
